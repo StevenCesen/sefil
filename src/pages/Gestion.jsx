2e3c84a45c2512e4fd1fa8c37a078b2c79662e63
Steven Cesen <steven.cesen@unl.edu.ec>
2024-05-07 10:17:57 -0500

    Actualización en estilos CSS

--- src/pages/Gestion.jsx
+++ src/pages/Gestion.jsx
@@ -18,6 +18,109 @@ export default function Gestion(){
                     }}
                 >Regresar</NavLink>
             </div>
+
+            <div className="Gestion">
+                <div className="Gestion__head">
+                    <div>
+                        <label>Nombre</label>
+                    </div>
+
+                    <div>
+                        <label>Cédula</label>
+                    </div>
+
+                    <div>
+                        <label>Agencia</label>
+                        <select>
+                            <option value={''}>--Todos--</option>
+                            <option value={"catacocha"}>CATACOCHA</option>
+                            <option value={"palanda"}>PALANDA</option>
+                            <option value={"cariamanga"}>CARIAMANGA</option>
+                            <option value={"zamora"}>ZAMORA</option>
+                            <option value={"zumba"}>ZUMBA</option>
+                            <option value={"piñas"}>PIÑAS</option>
+                            <option value={"celica"}>CELICA</option>
+                            <option value={"catamayo"}>CATAMAYO</option>
+                            <option value={"malacatos"}>MALACATOS</option>
+                            <option value={"santa rosa"}>SANTA ROSA</option>
+                            <option value={"oficina las pitas"}>OFICINA LAS PITAS</option>
+                            <option value={"oficina centro"}>OFICINA CENTRO</option>
+                            <option value={"oficina norte"}>OFICINA NORTE</option>
+                            <option value={"san miguel de los bancos"}>SAN MIGUEL DE LOS BANCOS</option>
+                            <option value={"milagro"}>MILAGRO</option>
+                            <option value={"santo domingo"}>SANTO DOMINGO</option>
+                            <option value={"el carmen"}>EL CARMEN</option>
+                            <option value={"cayambe"}>CAYAMBE</option>
+                            <option value={"pasaje"}>PASAJE</option>
+                            <option value={"tumbaco"}>TUMBACO</option>
+                            <option value={"la troncal"}>LA TRONCAL</option>
+                            <option value={"amaguaña"}>AMAGUAÑA</option>
+                            <option value={"naranjal"}>NARANJAL</option>
+                            <option value={"quinche"}>QUINCHE</option>
+                            <option value={"quininde"}>QUININDE</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label>Días de mora</label>
+                        <div>
+                            <div>
+                                <label>Min</label>
+                                <input type="number"/>
+                            </div>
+                            <div>
+                                <label>Max</label>
+                                <input type="number"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label>Monto</label>
+                        <div>
+                            <div>
+                                <label>Min</label>
+                                <input type="number"/>
+                            </div>
+                            <div>
+                                <label>Max</label>
+                                <input type="number"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label>Cuotas</label>
+                        <div>
+                            <div>
+                                <label>Min</label>
+                                <input type="number"/>
+                            </div>
+                            <div>
+                                <label>Max</label>
+                                <input type="number"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label>Estado</label>
+                        <select>
+                            <option value={"all"}>--Todos--</option>
+                            <option value={"Vencido"}>Vencidos</option>
+                            <option value={"Vigente"}>Vigentes</option>
+                            <option value={"Judicial"}>Judicial</option>
+                            <option value={"Prejudicial"}>Prejudicial</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label>Compromiso</label>
+                        <input type="date"/>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
