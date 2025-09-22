0fa97fff1e7360f129804529f074aa6906fc399d
StevenCesen <steven.cesen@unl.edu.ec>
2025-09-22 12:26:07 -0500

    Se esta modificando el CardStructure

--- src/components/CardStructure/CardStructure.jsx
+++ src/components/CardStructure/CardStructure.jsx
@@ -1,255 +1,194 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./CardStructure.css";
-import useFadeArray from "../../hooks/useFadeArray";
 import useStruct from "../../hooks/useStruct";
 import useFormatterNumber from "../../hooks/useFormatterNumber";
-import { useStoreStructure } from "../../stores/useStoreStructure";
+import useGenerateQuotes from "../../hooks/useGenerateQuotes";
 
-export default function CardStructure({credit_id,cartera,total_amount,gasto_cobranza}){
-    const [monto_cuota,setMonto]=useState();
+export default function CardStructure(){
     
-    const store_structure=useStoreStructure();
+    const [by_number_quote,setNumberQuote]=useState(0);
+    const [by_amount_quote,setAmountQuote]=useState(0);
+    const [agreement,setAgreement]=useState({
+        valor_cuota:0.00,
+        cuotas_pendientes:0,
+        cuota:0,
+        fecha:null,
+        credito:id,
+        cartera:cartera,
+        status:'',
+        detail:'',
+        original_dates:JSON.stringify(original_dates),
+        totalAmount:Number(total)+Number((status_cobranza!=='pay' | status_cobranza==true) ? cobranza : 0)
+    });
 
-    if(!store_structure.isViewOn) return <></>
+    const handlerCleanAgreement=({value,parameter_name})=>{
+        if(parameter_name==='number'){
+            setNumberQuote(value);
+            setAmountQuote(0);
+        }else{
+            setAmountQuote(value);
+            setNumberQuote(0);
+        }
+
+        setQuoteDetail([]);
+    };
+
+    const [quote_detail,setQuoteDetail]=useState([]);
 
     return (
         <div className="CardPay">
-            <button className="CardCondonacion__close" onClick={()=>{store_structure.viewOn(false)}}>Volver</button>
+            <button className="CardCondonacion__close" onClick={()=>{set()}}>Volver</button>
             <div className="CardStructure">
 
                 <p>Convenio de pago</p>
 
                 <div className="CardCondonacion__select">
                     <label>Saldo a desglozar:</label>
-                    <label>{useFormatterNumber({value:store_structure.total_amount,currency:'USD'})}</label>
-                </div>
-
-                <div className="CardCondonacion__select">
-                    <label>Desgloce:</label>
-                    <select value={store_structure.type} onChange={(e)=>{
-                        store_structure.setType(e.target.value);
-
-                        if(e.target.value==='manual'){
-                            store_structure.setChecksum(store_structure.total_amount)
-                            store_structure.setTotalFees(0);
-                        }
-                    }}>
-                        <option value="automatico">Cuotas iguales</option>
-                        <option value="manual">Cuotas diferentes</option>
-                    </select>
+                    <label>{useFormatterNumber({value:agreement.totalAmount,currency:'USD'})}</label>
                 </div>
 
                 <h4>Calcular por número de cuotas</h4>
 
                 <div className="CardCondonacion__select">
                     <label>Número de cuotas:</label>
-                    <input type="number" value={store_structure.total_fees} onChange={(e)=>{ 
-                        if(e.target.value>=0){
-                            store_structure.setTotalFees(e.target.value)
-                        }else{
-                            store_structure.setTotalFees(0);
-                        }
-
-                    }} min={1} step={1}/>
-                </div>
-                
-                <h4>Calcular número de cuotas por monto</h4>
-
-                <div className="CardCondonacion__selectThree">
-                    <label>Monto:</label>
-                    <input type="number" value={store_structure.amount_fee} onChange={(e)=>{ 
-                        if(e.target.value>=0){
-                            store_structure.setAmountFee(e.target.value);
-                        }else{
-                            store_structure.setAmountFee(0);
-                        }
-                    }} min={1} step={1}/>
-
-                    <button
-                        onClick={(e)=>{
-                            const nro_by_monto=(store_structure.total_amount)/store_structure.amount_fee;
-                            const cuotas_prev=[];
-
-                            useFadeArray(Math.round(nro_by_monto)).map((cuota,index)=>{
-                                if(index===(Math.round(nro_by_monto)-1)){
-                                    const last_quote=store_structure.total_amount-store_structure.amount_fee*(index);
-                                    cuotas_prev.push(last_quote);
-                                }else{
-                                    cuotas_prev.push(store_structure.amount_fee);
-                                }
-                            });
-                            store_structure.setFees(cuotas_prev);
-                            store_structure.setTotalFees(Math.round(nro_by_monto));
+                    <input 
+                        type="number" 
+                        value={by_number_quote} 
+                        onChange={(e)=>{ 
+                            if(e.target.value>=0){
+                                handlerCleanAgreement({
+                                    value:e.target.value,
+                                    parameter_name:'number'
+                                });
+                            }else{
+                                setNumberQuote(0);
+                            }
                         }}
-                    >Calcular</button>
+                        min={1} 
+                        step={1}
+                    />
+                </div>
+
+                <h4>Calcular por monto de cuota</h4>
+
+                <div className="CardCondonacion__select">
+                    <label>Monto de cuota:</label>
+                    <input 
+                        type="number" 
+                        value={by_amount_quote} 
+                        onChange={(e)=>{ 
+                            if(e.target.value>=0 & e.target.value<=agreement.totalAmount){
+                                handlerCleanAgreement({
+                                    value:e.target.value,
+                                    parameter_name:'amount'
+                                });
+                            }else{
+                                setAmountQuote(agreement.totalAmount);
+                            }
+                        }}
+                        min={1}
+                        max={agreement.totalAmount}
+                        step={1}
+                    />
+                </div>
+
+                <div className="CardCondonacion__select">
+                    <label>
+                        Fecha primer pago:
+                        <input
+                            type="date" 
+                            value={agreement.fecha} 
+                            onChange={(e)=>{ 
+                                setAgreement({
+                                    ...agreement,
+                                    fecha:e.target.value
+                                });
+                            }}
+                        />
+                    </label>
+                    <button
+                        className="CardStructure__button--generate"
+                        onClick={async (e)=>{
+                            setQuoteDetail([]);
+                            
+                            if(agreement.fecha=="" | agreement.fecha==null){
+                                
+                            }else{
+                                if(by_number_quote>0){
+                                    await useGenerateQuotes({
+                                        amount:agreement.totalAmount,
+                                        cobranza:cobranza,
+                                        status_cobranza:status_cobranza,
+                                        parameter_name:'number_quotes',
+                                        parameter_value:by_number_quote,
+                                        start_date:agreement.fecha,
+                                        setQuote:setQuoteDetail
+                                    });
+                                }else{
+                                    await useGenerateQuotes({
+                                        amount:agreement.totalAmount,
+                                        cobranza:cobranza,
+                                        status_cobranza:status_cobranza,
+                                        parameter_name:'amount_quotes',
+                                        parameter_value:by_amount_quote,
+                                        start_date:agreement.fecha,
+                                        setQuote:setQuoteDetail
+                                    });
+                                }
+                            }
+                        }}
+                    >
+                        Generar
+                    </button>
                 </div>
                 
                 {
-                    (store_structure.type==='automatico') ?
-                        <div className="CardCondonacion__quotes">
-
-                            <div>
-                                <label>1</label>
-                                <label>{useFormatterNumber({value:store_structure.gasto_cobranza,currency:'USD'})}</label>
-                                <label>Gastos de cobranza</label>
-                            </div>
-
-                            {
-                               useFadeArray(store_structure.total_fees).map((cuota,index)=>(
-
-                                    (store_structure.amount_fee>0 & store_structure.fees.length>0)
-                                    ?
-                                        <div key={index}>
-                                            <label>{index+2}</label>
-                                            <input className="desgloce_inputs" type="number" disabled value={ Number((store_structure.fees[index])).toFixed(2).replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/,'$1')}/>
-                                            <input type="date"/>
-                                        </div>
-                                    : 
-                                        <div key={index}>
-                                            <label>{index+2}</label>
-                                            <input className="desgloce_inputs" type="number" disabled value={(store_structure.total_amount/store_structure.total_fees).toFixed(2).replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/,'$1')}/>
-                                            <input type="date"/>
-                                        </div>
-                               )) 
-                            }
-                        </div>
-                    : 
-                        <div className="CardCondonacion__quotes">
-                            <div>
-                                <label>1</label>
-                                <label>{useFormatterNumber({value:store_structure.gasto_cobranza,currency:'USD'})}</label>
-                                <label>Gastos de cobranza</label>
-                            </div>
-
-                            {
-                               useFadeArray(store_structure.fees).map((cuota,index)=>(
-                                    <div key={index}>
-                                        <label>{index+2}</label>
-                                        <input
-                                            type="number"
-                                            className="desgloce_inputs"
-                                            placeholder="0.00"
-                                            step={0.01}
-                                            onChange={(e)=>{
-                                                let inputs=document.getElementsByClassName('desgloce_inputs');
-                                                inputs=[].slice.call(inputs);
-                                                
-                                                let val_prev=0,diferencia=0;
-
-                                                inputs.map((input)=>{
-                                                    diferencia=store_structure.total_amount-val_prev;
-                                                    val_prev+=Number(input.value);
-                                                });
-
-                                                if(val_prev>(store_structure.total_amount)){
-
-                                                    e.target.value=(diferencia-Number(store_structure.gasto_cobranza)).toFixed(2);
-                                                    
-                                                    store_structure.setChecksum(store_structure.total_amount);
-
-                                                    sendpush({
-                                                        title:'ERR: sumatoria incorrecta.',
-                                                        message:'Se sobrepaso el valor total del desgloce.',
-                                                        type:'Push--danger',
-                                                        timeout:3000
-                                                    });
-
-                                                }else{
-                                                    store_structure.setChecksum(val_prev);
-                                                }
-                                            }}
-                                        />
-                                        <input type="date"/>
-                                    </div>
-                               )) 
-                            }
-                        </div>
+                    <div className="CardCondonacion__quotes">
+                        {
+                            quote_detail.map((quote,index)=>(
+                                <div key={index}>
+                                    <label>{quote.cuota}</label>
+                                    <input
+                                        type="number"
+                                        className="desgloce_inputs"
+                                        placeholder="0.00"
+                                        step={0.01}
+                                        defaultValue={quote.valor}
+                                        onChange={(e)=>{
+                                            
+                                        }}
+                                        disabled
+                                    />
+                                    <input 
+                                        type="date" 
+                                        value={quote.fecha_pago}
+                                        disabled
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
                 }
 
-                {
-                    (store_structure.type==='manual')
-                    ?   
-                        <div className="CardCondonacion__select">
-                            <label>Suman:</label>
-                            <label>{useFormatterNumber({value:store_structure.checksum,currency:'USD'})}</label>
-                        </div>
-                    :   <></>
-                }
+                <button 
+                    className="CardStructure__button--save"
+                    onClick={async (e)=>{
+                        e.target.textContent="Guardando...";
 
-                <button onClick={(e)=>{
-
-                    // e.target.textContent="Guardando...";
-
-                    // const detalle=[];
-
-                    // let inputs=document.getElementsByClassName('desgloce_inputs');
-                    // inputs=[].slice.call(inputs);
-
-                    // let valor_cuota=0,errors=0;
-
-                    // detalle.push({
-                    //     cuota:1,
-                    //     valor:(status_cobranza!=='pay' ? cobranza : 0),
-                    //     estado:(status_cobranza!=='pay' ? 'PENDIENTE' : 'PAGADO'),
-                    //     fecha_pago:inputs[0].nextElementSibling.value
-                    // });
-                    
-                    // inputs.map((input,index)=>{
-                    //     if(index===0){
-                    //         valor_cuota=input.value;
-                    //     }
-
-                    //     if(input.nextElementSibling.value===''){
-                    //         errors++;
-                    //     }
-
-                    //     detalle.push({
-                    //         cuota:index+1,
-                    //         valor:input.value,
-                    //         estado:'PENDIENTE',
-                    //         fecha_pago:input.nextElementSibling.value
-                    //     });
-                    // });
-
-                    // const data={
-                    //     valor_cuota:valor_cuota,
-                    //     cuotas_pendientes:nro_cuotas-1,
-                    //     cuota:1,
-                    //     fecha:detalle[0].fecha_pago,
-                    //     credito:id,
-                    //     cartera:cartera,
-                    //     status:"",
-                    //     detail:JSON.stringify(detalle),
-                    //     original_dates:JSON.stringify(original_dates),
-                    //     totalAmount:totalAmount
-                    // }
-                    
-                    // if(errors>0){
-                    //     e.target.textContent="Guardar cambios";
-
-                    //     sendpush({
-                    //         title:'ERR: no hay fecha.',
-                    //         message:'Existen cuotas que no tienen fecha.',
-                    //         type:'Push--danger',
-                    //         timeout:3000
-                    //     });
+                        const data={
+                            ...agreement,
+                            detail:JSON.stringify(quote_detail),
+                            cuota:1,
+                            cuotas_pendientes:quote_detail.length-1,
+                            fecha:quote_detail[0].fecha_pago,
+                            valor_cuota:quote_detail[0].valor
+                        }
                         
-                    // }else if(tipo_desgloce==='automatico' | tipo_desgloce==='manual'){
-                        
-                    //     useStruct(data,e.target,id);
+                        const create_agreement=await useStruct(data,e.target,id);
 
-                    // }else{
-                    //     e.target.textContent="Guardar cambios";
+                    }}
+                >Guardar cambios</button> 
 
-                    //     sendpush({
-                    //         title:'ERR: sumatoria incorrecta.',
-                    //         message:'Se sobrepaso el valor total del desgloce.',
-                    //         type:'Push--danger',
-                    //         timeout:3000
-                    //     });
-                    // }
-
-                }}>Guardar cambios</button>
             </div>
         </div>
     );
